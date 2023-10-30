e53c7ce37e20d9cc92bb72062d6bb925d059c735
princebapu01 <jayveersinhchavda555@gmail.com>
2023-10-30 07:52:53 +0530

    Turbo boost with Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,56 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'slug' => 'my-work-post',
-            'title' => 'My Work Post',
-            'excerpt' => 'lorem is ipsum lorem is ipsum',
-            'body' => 'lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsumv lorem is ipsumlorem is ipsum',
-            'published_at' => now()
-        ]);
-
-        Post::create([
-            'category_id' => $family->id,
-            'user_id' => $user->id,
-            'slug' => 'my-family-post',
-            'title' => 'My Family Post',
-            'excerpt' => 'lorem is ipsum lorem is ipsum',
-            'body' => 'lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsumv lorem is ipsumlorem is ipsum',
-            'published_at' => now()
-        ]);
-
-        Post::create([
-            'category_id' => $personal->id,
-            'user_id' => $user->id,
-            'slug' => 'my-personal-post',
-            'title' => 'My Personal Post',
-            'excerpt' => 'lorem is ipsum lorem is ipsum',
-            'body' => 'lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsum lorem is ipsumv lorem is ipsumlorem is ipsum',
-            'published_at' => now()
-        ]);
-
+        $user = User::factory()->create(['name'=>'jaiveer chavda']);
+        Post::factory(5)->create(
+            [
+                'user_id' => $user->id,
+            ]
+        );
     }
 }
