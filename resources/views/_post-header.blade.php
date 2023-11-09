2c093c5834c40f931e9af152fe302554ccd52f78
princebapu01 <jayveersinhchavda555@gmail.com>
2023-11-09 13:02:15 +0530

    Extract a Drop down blade component.

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -15,47 +15,32 @@
         <div class="tw-flex tw-justify-center mt-8 space-y-2 lg:space-y-0 lg:space-x-4">
             <!-- Category -->
             <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
+                <x-dropdown>
 
-                <div x-data="{ show: false }" @click.away="show = false">
-                    <button
-                        @click="show = ! show"
-                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
-                    >
-                        {{isset($currentCategory) ? $currentCategory->name : 'Categories'}}
+                    {{-- trigger for displaying drop down link --}}
+                    <x-slot name="trigger">
+                        <button
+                            class="py-2 pl-3 pr-9 text-sm font-semibold
+                                w-full lg:w-32 text-left flex lg:inline-flex"
+                        >
 
-                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                            height="22" viewBox="0 0 22 22">
-                            <g fill="none" fill-rule="evenodd">
-                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                </path>
-                                <path fill="#222"
-                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                            </g>
-                        </svg>
-                    </button>
+                            {{ isset($currentCategory) ? $currentCategory->name : 'Categories' }}
 
-                    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
-                                <a
-                                    href="/"
-                                    class="block text-left px-3 text-sm leading-6 hover:bg-blue-500
-                                            focus:bg-blue-500 hover:text-white focus:text-white"
-                                >
-                                All
-                            </a>
+                            <x-icon name="down-arrow" class="absolute pointer-events-none"  style="right: 12px;"/>
+                        </button>
+                    </x-slot>
 
-                        @foreach ($categories as $category)
-                            <a
-                                href="/categories/{{ $category->slug }}"
-                                class="block text-left px-3 text-sm leading-6 hover:bg-blue-500
-                                        focus:bg-blue-500 hover:text-white focus:text-white
-                                        {{isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : ''}}
-                                        "
-                            >
-                            {{ ucwords($category->name)}}
-                            </a>
-                        @endforeach
-                    </div>
-                </div>
+                    {{-- drop down content(links) by default it will go into slot --}}
+
+                    <x-dropdown-item href="/" :active="request()->routeIs('home')"> All </x-dropdown-item>
+
+                    @foreach ($categories as $category)
+                    <x-dropdown-item
+                        href="/categories/{{$category->slug}}"
+                        :active="request()->is('categories/'.$category->slug)"
+                    >{{ ucwords($category->name) }}</x-dropdown-item>
+                    @endforeach
+                </x-dropdown>
             </div>
 
 
@@ -82,7 +67,8 @@
             <!--Other Filters -->
             <span class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2 ">
                 <form method="get" action="#">
-                    <input type="text" name="search" placeholder="find something" class="bg-transparent placeholder-black font-semibold text-sm">
+                    <input type="text" name="search" placeholder="find something"
+                        class="bg-transparent placeholder-black font-semibold text-sm">
                 </form>
             </span>
 
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
         'posts' => $posts,
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post}',function(Post $post) {
 
