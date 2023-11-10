c7cc13fa729195772442035d527925a859af5d74
princebapu01 <jayveersinhchavda555@gmail.com>
2023-11-10 10:27:23 +0530

    search (the messy way)

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $posts = Post::latest()->with('category','author')->get();
+
+    $posts = Post::latest();
+
+    if(request('search')){
+        $posts
+            ->where('title','like','%' . request('search') . '%')
+            ->Orwhere('body','like', '%' . request('search') . '%');
+    }
+
     return view('posts',[
-        'posts' => $posts,
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
+
 })->name('home');
 
 Route::get('/posts/{post}',function(Post $post) {
