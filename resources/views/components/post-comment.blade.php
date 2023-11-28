0abb4ad03b9090c39cb851e62aed70de9546e64a
princebapu01 <jayveersinhchavda555@gmail.com>
2023-11-28 15:34:44 +0530

    markup for post comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                 <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto odit soluta eaque qui hic vel molestias commodi libero, beatae
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,6 +49,15 @@
                             {!!$post->body!!}
                         </div>
                     </div>
+
+                    <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                        <x-post-comment/>
+                        <x-post-comment/>
+                        <x-post-comment/>
+                        <x-post-comment/>
+
+                    </section>
+
                 </article>
             </main>
         </section>
