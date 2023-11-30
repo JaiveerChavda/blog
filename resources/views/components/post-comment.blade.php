8c946163c2f3ab7ec2ee0c6ab8001d2ce590d7ae
princebapu01 <jayveersinhchavda555@gmail.com>
2023-11-30 10:21:15 +0530

    Design the comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,20 +1,22 @@
 @props(['comment'])
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100/u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/100/u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{$comment->author->name}}</h3>
-            <p class="text-xs">
-                Posted
-                 <time>{{$comment->created_at->diffForHumans()}}</time>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{$comment->author->name}}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{$comment->created_at->diffForHumans()}}</time>
+                </p>
+            </header>
+
+            <p>
+                {{$comment->body}}
             </p>
-        </header>
-
-        <p>
-            {{$comment->body}}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,6 +51,27 @@
                     </div>
 
                     <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                        <x-panel>
+                            <form method="POST" action="#">
+                                @csrf
+
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/100/u={{auth()->id()}}" alt="" width="40" height="40" class="rounded-full">
+
+                                    <h2 class="ml-4">Want to participate?</h2>
+                                </header>
+
+                                <div class="mt-6">
+                                    <textarea name="" id="" rows="5" placeholder="Quick, thing of something to say!" class="w-full text-sm focus:outline-non focus:ring"></textarea>
+                                </div>
+
+                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                    <button type="submit" class="bg-blue-500 text-white px-10 py-2 rounded-2xl text-xs font-semibold hover:bg-blue-600 uppercase">Post</button>
+                                </div>
+                            </form>
+                        </x-panel>
+
                         @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
                         @endforeach
