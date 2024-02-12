da423c50a2f3ec28f43677846bd8dfee97932fab
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-02-12 18:50:57 +0530

    show post not availabe message

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,40 +6,45 @@
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($posts as $post)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    <a href="/posts/{{ $post->slug }}">
-                                                        {{ $post->title }}
-                                                    </a>
+                                @if ($posts->count() > 0)
+                                    @foreach ($posts as $post)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        <a href="/posts/{{ $post->slug }}">
+                                                            {{ $post->title }}
+                                                        </a>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </td>
+                                            </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <span
-                                                class="px-3 rounded-2xl py-1 text-xs font-semibold {{ $post->status == 'published' ? 'bg-green-300' : 'bg-red-300' }} "
-                                            >
-                                            {{ $post->status }}
-                                        </span>
-                                        </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <span
+                                                    class="px-3 rounded-2xl py-1 text-xs font-semibold {{ $post->status == 'published' ? 'bg-green-300' : 'bg-red-300' }} "
+                                                >
+                                                {{ $post->status }}
+                                            </span>
+                                            </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="{{ route('admin.posts.edit',[$post]) }}" class="text-blue-500 hover:text-blue-600">Edit</a>
-                                        </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="{{ route('admin.posts.edit',[$post]) }}" class="text-blue-500 hover:text-blue-600">Edit</a>
+                                            </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <form method="POST" action="{{ route('admin.posts.destroy',[$post]) }}">
-                                                @csrf
-                                                @method('DELETE')
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <form method="POST" action="{{ route('admin.posts.destroy',[$post]) }}">
+                                                    @csrf
+                                                    @method('DELETE')
 
-                                                <button  class="text-xs text-gray-400">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                                    <button  class="text-xs text-gray-400">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                        <tr><td colspan="5"><em>No posts yet.</em></td></tr>
+                                @endif
+
                             </tbody>
                         </table>
                     </div>
