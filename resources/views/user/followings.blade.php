b0d4d7ccb43ae4eccb36692e2d38381bf812c7c8
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-07-04 22:38:22 +0530

    following author list layout - change

--- resources/views/user/followings.blade.php
+++ resources/views/user/followings.blade.php
@@ -10,18 +10,19 @@
 
             <div class="max-w-lg">
                 <p class="uppercase text-xs font-semibold mb-4">writers you follow</p>
-                @if ($followings->count()  > 0)
+                <div class="flex flex-col gap-3">
+                    @if ($followings->count()  > 0)
                     @foreach ($followings as $following)
-                        <p class="py-1">{{$following->name}}</p>
+                    <div class="flex gap-4 items-start justify-center border-b-2 py-2">
                         <p class="mb-2">{{$following->username}}</p>
                         <a href="{{ route('unfollow.author',[$following->username])}}"
                         class="bg-red-500 py-1 rounded-2xl text-white ml-auto px-5 text-sm font-bold">UnFollow</a>
-                        <hr class="mt-4">
+                    </div>
                     @endforeach
                 @else
                     <p class="text-center text-gray-500 py-8">You are not following any authors yet.</p>
                 @endif
-
+                </div>
             </div>
 
         </div>
