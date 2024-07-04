97fa246bd8a9c5ac08bdd02f9d07637118e3d257
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-07-04 22:18:35 +0530

    Button Component - merge additional css classes

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,4 +1,14 @@
-<button type="submit"
-    class="bg-blue-500 text-white px-10 py-2 rounded-2xl text-xs font-semibold hover:bg-blue-600 ">
+@props(['extClass'])
+@php
+    $class = 'px-10 py-2 rounded-2xl text-xs font-semibold';
+
+    $class .= $extClass ?? ' bg-blue-500 text-white  hover:bg-blue-600';
+
+@endphp
+
+<button type="submit" class="{{$class}}"
+    {{-- class="bg-blue-500 text-white px-10 py-2 rounded-2xl text-xs font-semibold hover:bg-blue-600 " --}}
+    {{-- class="px-10 py-2 rounded-2xl text-xs font-semibold border-solid border  hover:border-gray-300 border-gray-600" --}}
+    >
     {{$slot}}
 </button>
