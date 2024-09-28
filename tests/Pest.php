7a815b77b607a8f2f1a3600fcc01bdfe50c31f94
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-09-28 16:42:17 +0530

    test: add login helper method

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\Models\User;
+
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
@@ -41,7 +43,7 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
+function login($user = null) 
 {
-    // ..
+    return test()->actingAs($user ?? User::factory()->create());
 }
