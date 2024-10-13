7fc77df8f6af99f705efddf5505159040181c61f
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-10-13 16:10:28 +0530

    chore: remove unused methods from profile controller .

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
@@ -15,33 +14,10 @@ class ProfileController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('user.profile.index',[
-            'user' => $user
+
+        return view('user.profile.index', [
+            'user' => $user,
         ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
     }
 
     /**
@@ -49,8 +25,8 @@ class ProfileController extends Controller
      */
     public function edit(User $profile)
     {
-        return view('user.profile.edit',[
-            'user' => $profile
+        return view('user.profile.edit', [
+            'user' => $profile,
         ]);
     }
 
@@ -60,25 +36,17 @@ class ProfileController extends Controller
     public function update(User $profile)
     {
         $validated = request()->validate([
-            'name' => ['string','max:255'],
-            'username' => ['string','max:255',Rule::unique('users')->ignore($profile->id)],
-            'avatar' => ['nullable','image','max:2048'],
+            'name' => ['string', 'max:255'],
+            'username' => ['string', 'max:255', Rule::unique('users')->ignore($profile->id)],
+            'avatar' => ['nullable', 'image', 'max:2048'],
         ]);
 
-        if($validated['avatar'] ?? false){
+        if ($validated['avatar'] ?? false) {
             $validated['avatar'] = request()->file('avatar')->store('user/avatar');
         }
 
         $profile->update($validated);
 
-        return redirect()->route('profile.index')->with('success','profile updated successfully');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect()->route('profile.index')->with('success', 'profile updated successfully');
     }
 }
