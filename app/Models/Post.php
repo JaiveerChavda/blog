1c7cd340f6991b3ec798bc2912a6cc45b1de2516
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-11-24 16:59:30 +0530

    fix: post thumbnail issue

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use App\Enums\PostStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Feed\Feedable;
 use Spatie\Feed\FeedItem;
 
@@ -106,5 +108,21 @@ class Post extends Model implements Feedable
     public function getLink()
     {
         return route('post.show', $this);
+    }
+
+    public function thumbnail(): Attribute
+    {
+        return Attribute::make(
+            get:function ($value) {
+                if(is_string($value)){
+
+                    if(filter_var($value, FILTER_VALIDATE_URL)){
+                        return $value;
+                    }
+
+                    return Storage::disk('public')->url($value);
+                }
+            }
+        );
     }
 }
